b7a22dd4389c0ec7807fea5bb5d954e2bb39e7fa
Zaupa James <33663965+Kokiworse@users.noreply.github.com>
2019-01-26 00:05:24 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -1,3 +1,10 @@
+'''
+TODOs:
+- Implement drop of random pixels for image augmenting
+- Normalization because dataset is too large and it breaks
+'''
+
+
 import numpy as np
 import cv2
 from keras import Sequential
@@ -5,15 +12,19 @@ from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Flatten, Dense, Dr
 from decimal import *
 import matplotlib.pyplot as plt
 from keras.models import load_model
+from keras.optimizers import SGD
+from sklearn.preprocessing import StandardScaler
 
 TXT = "C:\\Users\\koki\\Desktop\\python\\comma\\speedchallenge\\data\\train.txt"
 img_rows = 150
 img_cols = 150
 timesteps = 16
+number_pixels = 150 * 150
+percent = round(number_pixels * 0.1)
 
 def model():
     model = Sequential()
-    input_shape=(timesteps, img_rows, img_cols, 1)
+    input_shape=(timesteps, img_rows, img_cols, 3)
 
     # 1st layer group
     model.add(TimeDistributed(Conv2D(32, (3, 3),  activation='relu', padding='same', name='conv1', input_shape=input_shape, kernel_initializer='glorot_uniform')))
@@ -40,6 +51,7 @@ def model():
     model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid', name='pool5')))
     model.add(TimeDistributed(Flatten()))
 
+    # FC
     model.add(Dense(512, activation='relu', name='fc6'))
     model.add(Dropout(.4))
     model.add(Dense(512, activation='relu', name='fc7'))
@@ -59,24 +71,49 @@ def normalize(X):
     X = np.array(X, dtype=np.float64)
     X -= np.mean(X)
     X /= np.std(X, axis = 0)
+    # for i in range(len(X)):
+    #     X = X / 255
     return X
 
+def augment(image):
+    rdm = np.random.randint(101)
+    #random flip
+    if rdm > 80:
+        image = cv2.flip(image, 1)
+    #brightness change
+    if rdm < 20:
+        bright = 0.5 + np.random.random(1)[0] * 0.5
+        hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        hsv[:,:,2] = hsv[:,:,2] * bright
+        image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+    #drop random pixels
+    if rdm > 40 and rdm < 60:
+        # for i in range(img_rows):
+        #     for j in range(img_cols):
+        pass
+    return image
+
 if __name__ == "__main__":
-    X = np.load('dataClean100gray.npz')
+    X = np.load('trainRGB.npz')
     X = X['arr_0']
-    #normalize
-    X = normalize(X)
-    print((X).shape)
+    print(X.shape)
+    c = 0
+    print("Normalized")
+    for i in range(len(X)):
+        rdm = np.random.randint(101)
+        if rdm < 25:
+            X[i] = augment(X[i])
+            c += 1
+    print("%d images changed" %c)
     model = model()
     y = readFromTxt()[ : len(X)]
     print(len(X), len(y))
     batch_size = X.shape[0] // timesteps
-    X = X.reshape(batch_size, timesteps, img_rows, img_cols, 1)
+    X = X.reshape(batch_size, timesteps, img_rows, img_cols, 3)
     y = y.reshape(batch_size, timesteps, 1)
-    print(y.shape)
-    print(X.shape)
+    # sgd = SGD(lr=1e-5, decay=0.0005, momentum=0.9)
     model.compile(optimizer='adam', loss='mse')
-    history = model.fit(X, y, validation_split=0.1, batch_size=8, shuffle=True, epochs=50, verbose=1, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
+    history = model.fit(X, y, validation_split=0.1, batch_size=8, shuffle=True, epochs=20, verbose=1, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
     # summarize history for loss
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -85,5 +122,5 @@ if __name__ == "__main__":
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
-    model.save('CNN-50epochs.h5')
+    model.save('CNN-20epochs.h5')
     print("Model saved")
