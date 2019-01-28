3509f91c555fe0988f6ac9684e9e033a00e714fa
Zaupa James <33663965+Kokiworse@users.noreply.github.com>
2019-01-28 02:05:12 +0100

    Hyperparams changes

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 '''
 TODOs:
-- Implement drop of random pixels for image augmenting
 - Normalization because dataset is too large and it breaks
 '''
 
@@ -13,21 +12,18 @@ from decimal import *
 import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.optimizers import SGD
-from sklearn.preprocessing import StandardScaler
 
 TXT = "C:\\Users\\koki\\Desktop\\python\\comma\\speedchallenge\\data\\train.txt"
 img_rows = 150
 img_cols = 150
-timesteps = 16
-number_pixels = 150 * 150
-percent = round(number_pixels * 0.1)
+timesteps = 20
 
 def model():
     model = Sequential()
     input_shape=(timesteps, img_rows, img_cols, 3)
 
     # 1st layer group
-    model.add(TimeDistributed(Conv2D(32, (3, 3),  activation='relu', padding='same', name='conv1', input_shape=input_shape, kernel_initializer='glorot_uniform')))
+    model.add(TimeDistributed(Conv2D(32, (3, 3),  activation='relu', padding='same', name='conv1', input_shape=input_shape, kernel_initializer='lecun_uniform')))
     model.add(TimeDistributed(MaxPooling2D(pool_size=(1, 2), strides=(1, 2), padding='valid', name='pool1')))
 
     # 2nd layer group
@@ -53,9 +49,9 @@ def model():
 
     # FC
     model.add(Dense(512, activation='relu', name='fc6'))
-    model.add(Dropout(.4))
+    model.add(Dropout(.45))
     model.add(Dense(512, activation='relu', name='fc7'))
-    model.add(Dropout(.4))
+    model.add(Dropout(.45))
     model.add(Dense(1, activation='linear', name='fc8'))
 
     return model
@@ -87,10 +83,13 @@ def augment(image):
         hsv[:,:,2] = hsv[:,:,2] * bright
         image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
     #drop random pixels
+    # so bad O(N*N) - better if I generate random indexes to drop --> O(N)
     if rdm > 40 and rdm < 60:
-        # for i in range(img_rows):
-        #     for j in range(img_cols):
-        pass
+        for i in range(img_rows):
+            for j in range(img_cols):
+                r1 = np.random.randint(101)
+                if r1 < 15:
+                    image[i,j,:] = 0
     return image
 
 if __name__ == "__main__":
@@ -98,22 +97,21 @@ if __name__ == "__main__":
     X = X['arr_0']
     print(X.shape)
     c = 0
-    print("Normalized")
     for i in range(len(X)):
         rdm = np.random.randint(101)
-        if rdm < 25:
+        if rdm < 30:
             X[i] = augment(X[i])
             c += 1
     print("%d images changed" %c)
     model = model()
-    y = readFromTxt()[ : len(X)]
+    y = readFromTxt()    #[ : len(X)] not full dataset
     print(len(X), len(y))
     batch_size = X.shape[0] // timesteps
     X = X.reshape(batch_size, timesteps, img_rows, img_cols, 3)
     y = y.reshape(batch_size, timesteps, 1)
     # sgd = SGD(lr=1e-5, decay=0.0005, momentum=0.9)
     model.compile(optimizer='adam', loss='mse')
-    history = model.fit(X, y, validation_split=0.1, batch_size=8, shuffle=True, epochs=20, verbose=1, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
+    history = model.fit(X, y, validation_split=0.1, batch_size=8, shuffle=True, epochs=30, verbose=1, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
     # summarize history for loss
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -122,5 +120,5 @@ if __name__ == "__main__":
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
-    model.save('CNN-20epochs.h5')
+    model.save('CNN-30epochs.h5')
     print("Model saved")
